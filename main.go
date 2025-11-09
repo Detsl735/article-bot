b5c2e710868afddfda5583478652755aab0b317c
Kirkange <korolev1324@gmail.com>
2025-11-09 13:14:15 +0300

    wrote a mustToken function to get a token from a flag at startup

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+)
 
+func main() {
+	t := mustToken()
+}
+
+func mustToken() string {
+	token := flag.String("token-bot-token", "", "token for access to telegram bot")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
 }

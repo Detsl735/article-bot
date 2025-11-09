584a2366224517f94ee5400d91344e59e1e1a220
Kirkange <korolev1324@gmail.com>
2025-11-09 14:18:53 +0300

    added creation thClient

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"flag"
 	"log"
+
+	"github.com/Detsl735/article-bot/clients/telegram"
+)
+
+const (
+	tgBotHost = "api.telegram.org"
 )
 
 func main() {
-	t := mustToken()
+	tgClient := telegram.New(tgBotHost, mustToken())
+
 }
 
 func mustToken() string {
